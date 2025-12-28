10c45ee00b1251995f017c8212f4f28a42a1ba5e
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2025-12-28 16:19:24 +0100

    feat: add delete user route (#4)

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { authorize } from '../middleware/auth';
 import { UserParams } from '../interface/user';
-import { createUser, CreateUserDTO, getUser } from '../service/user.service';
+import { CreateUserDTO, userService } from '../service/user.service';
 import { CustomError } from '../errors/CustomError';
 
 const router = Router();
@@ -18,7 +18,7 @@ router.get(
       if (Number.isNaN(userId)) {
         return res.status(400).json({ message: 'Invalid user id' });
       }
-      user = await getUser(userId);
+      user = await userService.getUser(userId);
     } catch (error: unknown) {
       if (error instanceof CustomError) {
         return res.status(error.code).json({ message: error.message });
@@ -36,7 +36,7 @@ router.post('/', authorize(['ADMIN']), async (req: Request, res: Response) => {
   const user: CreateUserDTO = req.body;
 
   try {
-    await createUser(user);
+    await userService.createUser(user);
   } catch (error: unknown) {
     if (error instanceof CustomError) {
       return res.status(error.code).json({ message: error.message });
@@ -50,9 +50,24 @@ router.post('/', authorize(['ADMIN']), async (req: Request, res: Response) => {
 });
 
 // DELETE /users/:id
-router.delete('/:id', authorize(['ADMIN']), (req: Request<UserParams>, res: Response) => {
-  const { id } = req.params;
-  res.status(200).json({ message: `User ${id} deleted` });
+router.delete('/:id', authorize(['ADMIN']), async (req: Request<UserParams>, res: Response) => {
+  const userId = Number(req.params.id);
+
+  try {
+    if (Number.isNaN(userId)) {
+      return res.status(400).json({ message: 'Invalid user id' });
+    }
+    await userService.deleteUser(userId);
+  } catch (error: unknown) {
+    if (error instanceof CustomError) {
+      return res.status(error.code).json({ message: error.message });
+    } else {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+  return res.status(200).json({ message: `User ${userId} deleted` });
 });
 
 export default router;
