e02ceec7cda24ee766fa9b0845b810050980dc73
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2025-12-31 18:24:44 +0100

    feat: use plantService to fetch plants in controller

--- src/routes/plant.routes.ts
+++ src/routes/plant.routes.ts
@@ -3,21 +3,15 @@ import { authorize } from '../middleware/auth';
 import { CustomError } from '../errors/CustomError';
 import { prisma } from '../prisma';
 import { SunlightLevel } from '../generated/prisma/enums';
+import { plantService } from '../service/plant.service';
 
 const router = Router();
 
 // GET /plants
 router.get('/', authorize(), async (req: Request, res: Response) => {
-  const sunlightQuery = req.query.sunlightLevel as string;
-  var plants = null;
-
   try {
-    if (!['FULL_SUN', 'PARTIAL_SHADE', 'SHADE'].includes(sunlightQuery)) {
-      return res.status(400).json({ message: 'Invalid sunlightLevel' });
-    }
-    plants = await prisma.plant.findMany({
-      where: { sunlightLevel: sunlightQuery as SunlightLevel },
-    });
+    const plants = await plantService.getPlants(req);
+    res.status(200).json(plants);
   } catch (error: unknown) {
     if (error instanceof CustomError) {
       return res.status(error.code).json({ message: error.message });
@@ -26,7 +20,6 @@ router.get('/', authorize(), async (req: Request, res: Response) => {
       return res.status(500).json({ message: 'Internal server error' });
     }
   }
-  res.status(200).json(plants);
 });
 
 export default router;
