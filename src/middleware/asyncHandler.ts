6c146588b4b5569405b590b6a2c8e60e5e067c84
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2026-01-04 15:04:27 +0100

    feat: add comment to asyncHandler

--- src/middleware/asyncHandler.ts
+++ src/middleware/asyncHandler.ts
@@ -6,6 +6,9 @@ type AsyncRouteHandler<TReq extends Request = Request> = (
   next: NextFunction,
 ) => void | Promise<void>;
 
+/**
+ * Middleware to handle async route handlers and pass errors to next()
+ */
 export const asyncHandler = (fn: AsyncRouteHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
